141e7cc3cbeb2845fc0add81dde0e3db4fc34d6c
ElPitagoras14 <jonfrgar@espol.edu.ec>
2020-12-29 18:24:30 -0500

    Add Logica

--- src/main/App.java
+++ src/main/App.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import system.BT;
 
 /**
  *
@@ -21,19 +22,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
@@ -47,5 +36,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
