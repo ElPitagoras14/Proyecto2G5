fdc088da20b0f58a6d0b3f70d224dec22becf499
ajvilleg10 <ajvilleg@espol.edu.ec>
2021-01-12 16:59:12 -0600

    Iniciando proyecto

--- src/main/App.java
+++ src/main/App.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import views.ventanaPrincipal;
 
 /**
  *
@@ -21,23 +22,14 @@ public class App extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        ventanaPrincipal vi= new ventanaPrincipal();          
+      
+       
+        Scene scene = new Scene(vi.getRoot(), 800, 500);
+        scene.getStylesheets().addAll(this.getClass().getResource("/src/Estilos.css").toExternalForm());               
+        primaryStage.setTitle("Genio Politecnico");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
